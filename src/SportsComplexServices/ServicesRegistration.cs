d08b3365624d14689435b5225882c6fd0e43a469
VladaBern <vladabernadskaya@gmail.com>
2023-08-31 16:37:17 +0300

    Added extension method for services registration

---
+++ src/SportsComplexServices/ServicesRegistration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SportsComplex.Services.Interfaces;
+using SportsComplex.Services.Validators;
+
+namespace SportsComplex.Services
+{
+    public static class ServicesRegistration
+    {
+        public static void AddServices(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddSingleton<IIdValidator, IdValidator>();
+            serviceCollection.AddSingleton<IDisciplineValidator, DisciplineValidator>();
+            serviceCollection.AddSingleton<ICoachValidator, CoachValidator>();
+            serviceCollection.AddSingleton<IClientValidator, ClientValidator>();
+            serviceCollection.AddTransient<IDisciplineService, DisciplineService>();
+            serviceCollection.AddTransient<ICoachService, CoachService>();
+            serviceCollection.AddTransient<IClientService, ClientService>();
+        }
+    }
+}
